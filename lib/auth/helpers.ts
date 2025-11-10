6c6bcce7502ef4a75ba55ab01a4a30deb7f836b4
hafiz <muhammad.mateen@amroodlabs.com>
2025-11-10 20:19:05 +0500

    ADDED: a new auth helper "getUserByEmail"

--- lib/auth/helpers.ts
+++ lib/auth/helpers.ts
@@ -1,4 +1,9 @@
 import bcrypt from "bcrypt";
+import User from "@/lib/models/user.model";
+
+export async function getUserByEmail(email: string) {
+  return await User.findOne({ email });
+}
 
 export async function verifyPassword(plain: string, hashed: string) {
   return await bcrypt.compare(plain, hashed);
