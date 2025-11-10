644fa4b320a824b99fb006a6839bf959625ea405
hafiz <muhammad.mateen@amroodlabs.com>
2025-11-10 20:21:10 +0500

    UPDATED: avoids sending password in response by default using "select: false", to avoid accidently data(password) breachs.

--- lib/auth/helpers.ts
+++ lib/auth/helpers.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import User from "@/lib/models/user.model";
 
 export async function getUserByEmail(email: string) {
-  return await User.findOne({ email });
+  return await User.findOne({ email }).select("+password");
 }
 
 export async function verifyPassword(plain: string, hashed: string) {
--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -32,7 +32,7 @@ const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     role: { type: String, enum: ["investor", "founder", "admin"], required: true },
     profileImage: { type: String },
     bio: { type: String },
