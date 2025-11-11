b23d9411994fe6ba093c8f632ed354015cc04b5a
qurrahazam <qurrahazam@gmail.com>
2025-11-11 18:04:38 +0500

    ADDED: types for startup

---
+++ app/types/startup.ts
@@ -0,0 +1,22 @@
+export interface Startup {
+  _id: string;
+  title: string;
+  description: string;
+  pitch: string;
+  founders: string[];
+  investors: string[];
+  badges: string[];
+  categoryType: string;
+  industry: string;
+  socialLinks: {
+    website?: string;
+    linkedin?: string;
+    twitter?: string;
+  };
+  followers: number;
+  status: string;
+  ratingCount: number;
+  avgRating: number;
+  equityRange: { range: string; equity: number }[];
+  profilePic: string;
+}
