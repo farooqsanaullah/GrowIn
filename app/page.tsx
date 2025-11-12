f2a1014c33b521af12b406e96bb44f5cec4e1c0c
m-faisal930 <faisal.muhammad7363@gmail.com>
2025-11-12 10:40:58 +0500

    FIXED: formatting of code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Footer from '@/components/landingpage/Footer'
-import Header from '@/components/landingpage/Header'
-import HeroSection from '@/components/landingpage/HeroSection'
-import HowToInvest from '@/components/landingpage/HowToInvest'
-import FAQs from '@/components/landingpage/FAQs'
-import Testimonial from '@/components/landingpage/Testimonial'
+import Footer from "@/components/landingpage/Footer";
+import Header from "@/components/landingpage/Header";
+import HeroSection from "@/components/landingpage/HeroSection";
+import HowToInvest from "@/components/landingpage/HowToInvest";
+import FAQs from "@/components/landingpage/FAQs";
+import Testimonial from "@/components/landingpage/Testimonial";
 
 function Home() {
   return (
-    <div className='font-montserrat'>
+    <div className="font-montserrat">
       <Header />
       <HeroSection />
       <HowToInvest />
@@ -15,7 +15,7 @@ function Home() {
       <Testimonial />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
