60983d40383a99cb5bab5aed626f93f948aeacd7
hafiz <muhammad.mateen@amroodlabs.com>
2025-11-18 14:48:04 +0500

    FIXED: separate helpers for frontend & backend to avoid conflicts of node(server) based only modules (fs/bcrypt,db, jwt)

---
+++ lib/helpers/backend/index.ts
@@ -0,0 +1,4 @@
+export * from "@/lib/helpers/backend/auth";
+export * from "@/lib/helpers/backend/jwt";
+export * from "@/lib/helpers/backend/session";
+export * from "@/lib/helpers/backend/user";
--- lib/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "@/lib/helpers/auth";
-export * from "@/lib/helpers/jwt";
-export * from "@/lib/helpers/session";
-export * from "@/lib/helpers/user";
-export * from "@/lib/helpers/validation";
