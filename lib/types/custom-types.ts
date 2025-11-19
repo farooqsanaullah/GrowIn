5156fcf7f3839466aa260b8b5481fb9c5d619e1d
hafiz <muhammad.mateen@amroodlabs.com>
2025-11-19 14:26:02 +0500

    DELETED: custom-types bcz all form errors are now handled by RHF itself

--- lib/types/custom-types.ts
+++
@@ -1,9 +0,0 @@
-export type FormErrors = {
-  email?: string;
-  password?: string;
-  currentPassword?: string;
-  newPassword?: string;
-  confirmPassword?: string;
-  userName?: string;
-  formError?: string;
-};
