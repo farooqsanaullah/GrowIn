51e4a2ee5c4788d5d5266b4cc79a5770b4ce18fa
hafiz <muhammad.mateen@amroodlabs.com>
2025-11-20 20:16:57 +0500

    UPDATED: integrated zod in signup route

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,14 +1,9 @@
 import { NextRequest } from "next/server";
 import { connectDB } from "@/lib/db/connect";
-import { 
-  validateEmail, 
-  validatePassword, 
-  validateRole, 
-  validateUsername,
-} from "@/lib/helpers/shared";
 import { hashPassword, getUserByEmail } from "@/lib/helpers/backend";
 import { success, error } from "@/lib/auth/apiResponses";
 import User from "@/lib/models/user.model";
+import { signupSchema } from "@/lib/auth/zodValidationSchema";
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -17,24 +12,21 @@ export async function POST(req: NextRequest) {
     await connectDB();
 
     const body = await req.json();
-    const { userName, email, password, role } = body;
 
-    // --- Basic validation ---
-    if (!userName || !email || !password || !role) return error("userName, email, password, and role are required", 400);
+    // Validating request body with zod
+    const parsed = signupSchema.safeParse(body);
+    if (!parsed.success) {
+      return error(parsed.error.issues[0].message, 400);
+    }
 
-    // --- Check if user already exists ---
-    const validationError = validateEmail(email);
-    if (validationError) return error(validationError, 400);
-    if (await getUserByEmail(email)) return error("User already exists", 409);
-    
-    // --- Validation ---
-    const userNameError = validateUsername(userName);
-    if (userNameError) return error(userNameError, 400);
-    const passwordError = validatePassword(userName);
-    if (passwordError) return error(passwordError, 400);
-    const roleError = validateRole(userName);
-    if (roleError) return error(roleError, 400);
+    const { userName, email, password, role } = parsed.data;
 
+    // Check existing user
+    if (await getUserByEmail(email)) {
+      return error("User already exists", 409);
+    }
+
+    // Hash password & create user
     const hashedPassword = await hashPassword(password);
 
     // --- Create new user ---
