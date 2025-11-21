2a6e1cf2700bcae8e4690addce40e0707bea3d93
m-faisal930 <faisal.muhammad7363@gmail.com>
2025-11-21 17:52:19 +0500

    UPDATED: central export file

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -4,3 +4,7 @@ export { TopBar } from "./TopBar";
 export { StatsCard } from "./StatsCard";
 export { DashboardStats } from "./DashboardStats";
 export { RecentStartups } from "./RecentStartups";
+export { AnalyticsSection } from "./AnalyticsSection";
+export { StartupPerformance } from "./StartupPerformance";
+export { RecentActivity } from "./RecentActivity";
+export { GrowthInsights } from "./GrowthInsights";
