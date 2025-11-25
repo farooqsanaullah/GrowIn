4dc2de21819f1733bcf0ad6d4ea41ace0aea338b
hafiz <muhammad.mateen@amroodlabs.com>
2025-11-25 17:49:46 +0500

    ADDED: PHONE_REGEX in global constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,4 @@ export const USERNAME_MAX_LENGTH = 30;
 export const PASSWORD_MIN_LENGTH = 8;
 export const ALLOWED_ROLES = ["investor", "founder"] as const;
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const PHONE_REGEX = /^\+?[0-9\s()-]{7,20}$/;
