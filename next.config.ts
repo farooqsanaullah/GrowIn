eef7b1b814a86d43bda2f7c4d3b0dd3db63c2e3e
hafiz <muhammad.mateen@amroodlabs.com>
2025-11-25 16:54:15 +0500

    FIXED: next/image external domain warning by using remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "", // leaving empty for default
+        pathname: "/**", // allows all paths
+      },
+      // We can add more patterns here for other hosts
+    ],
+  },
 };
 
 export default nextConfig;
