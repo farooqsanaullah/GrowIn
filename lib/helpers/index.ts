4936d01b0fb59d9f1e8384a1841b70343aad3c5b
hafiz <muhammad.mateen@amroodlabs.com>
2025-12-03 21:38:24 +0500

    FIXED: fs error node dependent exports now the are seprate

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -2,12 +2,6 @@
 export * from "@/lib/helpers/api/handlers";
 export * from "@/lib/helpers/api/startups";
 
-// Backend
-export * from "@/lib/helpers/backend/auth";
-export * from "@/lib/helpers/backend/jwt";
-export * from "@/lib/helpers/backend/session";
-export * from "@/lib/helpers/backend/user";
-
 // Stripe
 export * from "@/lib/helpers/stripe/sendInvestmentEmails";
 
