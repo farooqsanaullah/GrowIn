b10dd3773c931485529f9c12a94f01fa67aa04e0
m-faisal930 <faisal.muhammad7363@gmail.com>
2025-12-04 12:43:01 +0500

    ADDED: investments compound indexing

--- lib/models/investment.model.ts
+++ lib/models/investment.model.ts
@@ -1,5 +1,8 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+import "./user.model";
+import "./startup.model";
+
 export interface IInvestment extends Document {
   investorId: mongoose.Types.ObjectId;
   startupId: mongoose.Types.ObjectId;
@@ -28,5 +31,24 @@ const InvestmentSchema = new Schema<IInvestment>({
   refundedAt: { type: Date },
 }, { timestamps: true });
 
-const Investment = mongoose.models.Investment || mongoose.model<IInvestment>('Investment', InvestmentSchema);
+InvestmentSchema.index({ investorId: 1, startupId: 1 }, { unique: true });
+
+
+InvestmentSchema.index({ investorId: 1, createdAt: -1 });
+
+
+InvestmentSchema.index({ startupId: 1, createdAt: -1 });
+
+
+InvestmentSchema.index({ investorId: 1, amount: -1 });
+
+
+InvestmentSchema.index({ startupId: 1, amount: -1 });
+
+
+InvestmentSchema.index({ amount: -1, createdAt: -1 });
+
+
+const Investment = mongoose.models.Investment || mongoose.model<IInvestment>("Investment", InvestmentSchema);
+
 export default Investment;
