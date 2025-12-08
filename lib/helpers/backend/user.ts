ab19178d143578f59ce04589e9f5592682b2c55c
hafiz <muhammad.mateen@amroodlabs.com>
2025-12-08 16:28:37 +0500

    ADDED: getUsersByIds helper to fetch all investors

--- lib/helpers/backend/user.ts
+++ lib/helpers/backend/user.ts
@@ -4,6 +4,10 @@ export async function getUserById(userId: string) {
   return await User.findById(userId);
 }
 
+export async function getUsersByIds(ids: string[]) {
+  return User.find({ _id: { $in: ids } }).select("email");
+}
+
 export async function getUserByEmail(email: string) {
   return await User.findOne({ email }).select("+password");
 }
