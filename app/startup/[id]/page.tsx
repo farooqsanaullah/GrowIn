bc69fb1a1ec54d9454d1989dd95117ca1f68a55c
hafiz <muhammad.mateen@amroodlabs.com>
2025-12-23 14:14:59 +0500

    FIXED: params must be awaited while fetching startup based in id

--- app/startup/[id]/page.tsx
+++ app/startup/[id]/page.tsx
@@ -2,16 +2,17 @@ import { Startup } from "@/lib/types/startup";
 import ClientWrapper from "@/components/startup/ClientWrapper";
 import { notFound } from "next/navigation";
 
-
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 const StartupProfilePage = async ({ params }: Props) => {
-  const { id } = params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/startups/${id}`, {
-    cache: "no-store",
-  });
+  const { id } = await params;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/startups/${id}`,
+    { cache: "no-store" }
+  );
 
   if (!res.ok) notFound();
 
